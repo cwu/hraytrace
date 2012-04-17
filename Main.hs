18bf40db42f9a119108128e61db25a58934f41d2
Jack Huang <jackhxs@gmail.com>
2012-04-17 17:40:44 -0400

    added cubes

--- Main.hs
+++ Main.hs
@@ -10,10 +10,10 @@ k = V 0 0 1
 o = V 0 0 0
 
 eye = o
-hit = Ray (V 0.95 0 0) (-k)
-miss = Ray eye i
+hit = Ray eye (norm ((V (-1.7) 0.3 (-3.4)) - eye))
+miss = Ray eye (norm ((V 0 1.2 (-4)) - eye))
 
-screen = Screen 600 300 45 (1/2)
+screen = Screen 400 200 45 (1)
 camera = Camera eye (-k) j
 
 
@@ -21,9 +21,11 @@ sphere1 = Sphere (V 0 0 (-4)) 1
 sphere2 = Sphere (V 2.5 0 (-4)) 0.5
 material1 = Material (Color 1.0 0.0 0.0) (Color 1 1 1) 20
 material2 = Material (Color 0.0 1.0 0.0) (Color 1 1 1) 20
-scene = Scene [Object sphere1 material1, Object sphere2 material2]
-light = Light (V 1300 0 (-4)) (Color 0.8 0.8 0.8) (Color 0.8 0.8 0.8)
-world = World scene [light] (Color 0.3 0.3 0.3)
+material3 = Material (Color 0.0 0.0 1.0) (Color 1 1 1) 20
+cube = Cube (V (-2) 0.3 (-4)) 0.8
+scene = Scene [Object sphere1 material1, Object sphere2 material2, Object cube material3]
+light = Light (V 1300 200 200) (Color 0.8 0.8 0.8) (Color 0.8 0.8 0.8)
+world = World scene [light] (Color 0.1 0.1 0.1)
 
 -- debug stuff
 rt = rayTrace world eye
